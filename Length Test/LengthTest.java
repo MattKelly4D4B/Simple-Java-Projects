9cbd4055c3d1a300f13ed5934fc525c2dab95798
Matt Kelly <105619429+MattKelly4D4B@users.noreply.github.com>
2022-05-16 00:17:44 -0400

    Add files via upload

---
+++ Length Test/LengthTest.java
@@ -0,0 +1,9 @@
+public class LengthTest
+{
+  public static void main(String[] args)
+  {
+    //display a greeting in the console window
+
+    System.out.println("Hello, World!" .length());
+  }
+}
